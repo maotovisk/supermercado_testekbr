a78de80b97289e9dc5224209f05fca1437c0859f
maotovisk <maotgames@gmail.com>
2021-02-25 02:14:29 -0300

    Added Products migration and model

---
+++ database/migrations/2021_02_25_015821_create_produtos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo')->unique();
+            $table->string('descricao');
+            $table->integer('subcategoria_id');
+            $table->string('imagem');
+            $table->decimal('valor');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
