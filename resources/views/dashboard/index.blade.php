eb539bc6e9489a6938c503d56a75516f5305a6dc
maotovisk <maotovisk@github.com>
2021-02-28 22:52:59 -0300

    crearing debug messages

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -13,7 +13,6 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        {{ Auth::user()->id }}
                         <br>
                         @if (Auth::user()->is_admin)
                             Olá administrador, para começar a navegar, utilize das abas acima!
